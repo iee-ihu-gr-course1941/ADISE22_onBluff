f8b3af96d3df975872357a6745df9cec29ffc6f8
Thanos Christou <thanoschri.it@gmail.com>
2022-12-12 22:59:31 +0200

    update bluff.php and game.php

--- www/bluff.php
+++ www/bluff.php
@@ -1,6 +1,7 @@
 <?php
  require_once "../lib/dbconnect.php";
  require_once "../lib/game_board.php";
+ require_once "../lib/game.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
@@ -11,33 +12,42 @@ $input = json_decode(file_get_contents('php://input'),true);
 
 switch ($r=array_shift($request)) {
     case 'game_board' :
-        switch ($b=array_shift($request)){
-    case '':
-        case null: handle_board($method);break;
+    switch ($b=array_shift($request)){
+        case '':
+        case null: handle_board($method);
+        break;
         case 'piece': handle_piece($method, $request[0],$request[1],$input);
-                                    break;
+                    break;
         case 'player': handle_player($method, $request[0],$input);
-                                    break;
+                    break;
         default: header("HTTP/1.1 404 Not Found");
-                                    break;
-}
-break;
-    case 'hands' :
-        switch ($b=array_shift($request)){
-    case '':
-        case null: handle_hands($method);break;
-        case 'piece': //handle_piece($method, $request[0],$request[1],$input);
-                                    break;
-        case 'player': //handle_player($method, $request[0],$input);
-                                    break;
-        default: header("HTTP/1.1 404 Not Found");
-                                    break;
+                    break;
+
     }
-break;
-default:
-    header("HTTP/1.1 404 Not Found");
-    exit;
+            break;
+     case 'status':
+        if (sizeof($request)==0) {handle_status($method);}
+        else {header("HTTP/1.1 404 Not Found");}
+        break;
+    case 'hands' :
+            switch ($b=array_shift($request)){
+        case '':
+            case null: handle_hands($method);break;
+            case 'piece': //handle_piece($method, $request[0],$request[1],$input);
+                                        break;
+            case 'player': //handle_player($method, $request[0],$input);
+                                        break;
+            }
+    case 'players': handle_player($method, $request, $input);
+    break;   
+        default: header("HTTP/1.1 404 Not Found");
+                                    break;
+                                 
 }
+        
+    exit;
+
+
 
 
 
@@ -70,7 +80,7 @@ function handle_player($method, $p,$input){
 
 function handle_status($method){
     if($method == 'GET'){
-        show_status();
+        show_game_status();
     } else {
         header('HTTP/1.1405 Method Not Allowed');
     }
