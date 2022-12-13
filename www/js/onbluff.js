0905aab3e44f7ba35619322277ed0dfe052bac43
Thanos Christou <thanoschri.it@gmail.com>
2022-12-13 05:05:55 +0200

    update html

---
+++ www/js/onbluff.js
@@ -0,0 +1,7 @@
+function loadGame(){
+    $("#game_board").css("background-image", "url(img/game_baord.jpg)");
+}
+$("#test").click = function(){
+    alert("Hello World");
+};
+// $("#game_reset").click(function(){alert("hi");});
